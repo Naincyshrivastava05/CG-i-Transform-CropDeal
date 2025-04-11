768a28537aadbc8213ecbc9b3b5fe3891a70638e
Naincyshrivastava05 <naincyshri05@gmail.com>
2025-04-11 13:38:10 +0530

    Remove dealer-service from farmer-service branch

--- dealer-service/src/main/java/com/cropdeal/dealer_service/repository/DealerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cropdeal.dealer_service.repository;
-
-import com.cropdeal.dealer_service.model.Dealer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DealerRepository extends JpaRepository<Dealer, Long> {
-}
